a363b41e97ea561a0a05d7d2e479ce2fbe79c647
hammad058 <56123908+hammad058@users.noreply.github.com>
2023-06-22 00:49:24 +0500

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,6 @@
+// Toggle the navigation bar when the toggle button is clicked
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    navbar.classList.toggle('show');
+  });
+  
